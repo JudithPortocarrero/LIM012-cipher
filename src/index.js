0e31fc994ebc6f5219943f63f27b6fe0afb0ae51
Your Name <judithportocarrero99@gmail.com>
2020-02-17 17:26:57 -0500

    judithportocarrero

--- src/index.js
+++ src/index.js
@@ -1,24 +1,45 @@
 import cipher from './cipher.js';
-
-//let form = document.querySelector("form");
-//let texto = document.getElementById("texto").value;
-//let offset = document.getElementById("offset").value;
+document.getElementById("todo2").style.display="none";
+document.getElementById("todo1").style.display="none";
+document.getElementById("inicio").style.display="none";
+const indexco = document.getElementById("indexco").addEventListener("click",indexcodi);
+const indexde = document.getElementById("indexde").addEventListener("click",indexdeco);
 let codificar = document.getElementById("codificar").addEventListener("click",respuestaImpresa);
+let inicio = document.getElementById("inicio").addEventListener("click",inicioI);
 let decodificar = document.getElementById("decodificar").addEventListener("click",respuestaImpresa2);
 let codificado = document.getElementById("codificado");
-//let cip = cipher.encode(texto,offset);
-//console.log(cip);
-//const codificado = document.getElementById("codificado");
-//const palabra = texto.length;
-//let result, ascii;
+let codificado1 = document.getElementById("codificado1");
+
 function respuestaImpresa(){
     let todo = cipher.encode(document.getElementById("texto").value,document.getElementById("offset").value);
     codificado.innerHTML="En un lenguaje secreto "+document.getElementById("texto").value+" significa:"+
     todo;
 }
 function respuestaImpresa2(){
-    let todo = cipher.decode(document.getElementById("texto").value,document.getElementById("offset").value);
-    codificado.innerHTML="En un lenguaje secreto "+document.getElementById("texto").value+" significa:"+
+    let todo = cipher.decode(document.getElementById("texto1").value,document.getElementById("offset1").value);
+    codificado1.innerHTML="En un lenguaje secreto "+document.getElementById("texto1").value+" significa:"+
     todo;
 }
 //------------------------------------------------------------------------------------
+function indexcodi() {
+    document.getElementById("todo2").style.display="none";
+    document.getElementById("decision").style.display="none";
+    document.getElementById("todo1").style.display="block";
+  	document.getElementById("inicio").style.display="block";
+}
+function indexdeco() {
+    document.getElementById("todo1").style.display="none";
+    document.getElementById("decision").style.display="none";
+    document.getElementById("todo2").style.display="block";
+  	document.getElementById("inicio").style.display="block";
+}
+function inicioI() {
+    document.getElementById("todo1").style.display="none";
+    document.getElementById("todo2").style.display="none";
+    document.getElementById("decision").style.display="block";
+    document.getElementById("inicio").style.display="none";
+    codificado.innerHTML="";
+    codificado1.innerHTML="";
+    document.getElementById("texto").value="";
+    document.getElementById("offset").value="";
+}
