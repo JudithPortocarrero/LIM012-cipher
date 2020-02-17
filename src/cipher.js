235a4c523358acf55ddafddf78871dbffa27bf75
Your Name <judithportocarrero99@gmail.com>
2020-02-17 12:34:16 -0500

    judithportocarrero

--- src/cipher.js
+++ src/cipher.js
@@ -2,45 +2,41 @@ const cipher = {
 }
   
   cipher.encode = function (texto, offset){
+    const offset1=parseInt(offset,10);
     let longitud = texto.length;
-    let cifrado='', ascii, resultado;
+    let cifrado='';
     for(let i=0 ; i<longitud ; i++){
       let ascii = texto.charCodeAt(i);
       if(ascii>=65 && ascii<=90){
-        resultado = (ascii-65+offset)%26+65;
-        cifrado += String.fromCharCode(resultado);
-        console.log(cifrado);
+        cifrado += String.fromCharCode((ascii-65+offset1)%26+65);
       }
       else if(ascii>=97 && ascii<=122){
-        resultado = (ascii-97+offset)%26+97;
-        cifrado += String.fromCharCode(resultado);
-        console.log(cifrado);
+        cifrado += String.fromCharCode((ascii-97+offset1)%26+97);
       }
       else if(ascii===32){
-        console.log(" ");
+        cifrado+=" ";
       }
     }
     return cifrado;
   };
 
   cipher.decode = function (texto, offset){
+    const offset1=parseInt(offset,10);
     let longitud = texto.length;
-    let decifrado, ascii2, resultado2;
+    let decifrado='';
     for(let j=0; j<longitud; j++){
       let ascii2 = texto.charCodeAt(j);
       if(ascii2>=65 && ascii2<=90){
-        let resultado2 = (ascii2-90-offset)%26+90;
-        decifrado = String.fromCharCode(resultado2);
-        console.log(decifrado);
+        decifrado += String.fromCharCode((ascii2-90-offset1)%26+90);
       }
       else if(ascii2>=97 && ascii2<=122){
-        let resultado2 = (ascii2-122-offset)%26+122;
-        decifrado = String.fromCharCode(resultado2);
-        console.log(decifrado);
+        decifrado += String.fromCharCode((ascii2-122-offset1)%26+122);
       }
       else if(ascii2===32){
-        console.log(" ");
+        decifrado += " ";
       }
     }
-};
+    return decifrado;
+  };
 export default cipher;
+      
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import cipher from './cipher.js';
 
-const form = document.querySelector("form");
-const texto1 = document.getElementById('texto').value;
-const offset1 = document.getElementById('offset').value;
-const codificar = document.getElementById("codificar").addEventListener("click",respuestaImpresa);
-var codificado = document.getElementById("codificado");
-let cip = cipher.encode(texto1,offset1);
-console.log(cip);
+//let form = document.querySelector("form");
+//let texto = document.getElementById("texto").value;
+//let offset = document.getElementById("offset").value;
+let codificar = document.getElementById("codificar").addEventListener("click",respuestaImpresa);
+let decodificar = document.getElementById("decodificar").addEventListener("click",respuestaImpresa2);
+let codificado = document.getElementById("codificado");
+//let cip = cipher.encode(texto,offset);
+//console.log(cip);
 //const codificado = document.getElementById("codificado");
 //const palabra = texto.length;
 //let result, ascii;
 function respuestaImpresa(){
-    codificado.innerHTML="En un lenguaje secreto "+texto1+"significa:"+cip;
+    let todo = cipher.encode(document.getElementById("texto").value,document.getElementById("offset").value);
+    codificado.innerHTML="En un lenguaje secreto "+document.getElementById("texto").value+" significa:"+
+    todo;
 }
+function respuestaImpresa2(){
+    let todo = cipher.decode(document.getElementById("texto").value,document.getElementById("offset").value);
+    codificado.innerHTML="En un lenguaje secreto "+document.getElementById("texto").value+" significa:"+
+    todo;
+}
+//------------------------------------------------------------------------------------
